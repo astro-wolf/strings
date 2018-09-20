c0eaa77ff5c067ef92dbf4c024addf06c60a4e52
Vishwas <vishwas@kirana11.com>
2018-09-20 19:05:15 +0530

    Implemented valid palindrome

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,5 +1,26 @@
 public class ValidPalindrome {
 
+    public static void main(String[] args) {
+        ValidPalindrome validPalindrome = new ValidPalindrome();
+        boolean isValid = validPalindrome.isPalindrome("A man, a plan, a canal: Panama");
+        System.out.print(isValid);
+    }
 
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < s.length() - 1 && !Character.isDigit(s.charAt(i)) && (!Character.isAlphabetic(s.charAt(i)) || s.charAt(i) == ' '))
+                i++;
+            while (j > 0 && !Character.isDigit(s.charAt(j))  && (!Character.isAlphabetic(s.charAt(j)) || s.charAt(j) == ' '))
+                j--;
+            if(i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+            if(i < j) {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
 
 }
